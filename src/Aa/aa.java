0657caa562c5960c642376ff6ad49adeb0d72ff1
FYMYYDS <92851923+FYMYYDS@users.noreply.github.com>
2021-11-24 20:15:11 +0800

    Update aa.java
    修改了

--- src/Aa/aa.java
+++ src/Aa/aa.java
@@ -3,7 +3,7 @@ package Aa;
 public class aa {
 	public static void main(String[] args) {
 		
-		System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+		System.out.println("bbbbbbbbbbbbbbbbbbbbbbbbb");
 		System.out.println(51546);
 		
 	}
